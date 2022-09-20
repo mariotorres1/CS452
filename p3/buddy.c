5dbda5da6ca57fc2cc133f7f205c0976f6f9bcc3
Mario Torres <mariotorres791@u.boisestate.edu>
2022-09-19 20:48:35 -0600

    Add functionality for buddy_init()

--- p3/buddy.c
+++ p3/buddy.c
@@ -1,11 +1,40 @@
 /**
  * Implementation of a memory allocator based on the Buddy System.
+ * Author: Mario Torres
+ * Date: Mon Sep 19 19:15:51 MDT 2022
  */
  
 #include "buddy.h"
 
 int buddy_init(void) { 
-	return FALSE;
+	// Getting size of memory
+	base = sbrk(DEFAULT_MAX_MEM_SIZE);
+
+	// Creating header pointer
+	struct block_header *p = (struct block_header *) base;
+
+	// Update information for each array position
+	for (int i = 0; i <= 29; i++) {
+		// Updating final position's information
+		if (i == 29) {
+			avail[i].next = p;
+			avail[i].prev = p;
+			avail[i].kval = i;
+			avail[i].tag = UNUSED;
+		} else // Upading position's 0 - 28 information
+		avail[i].next = &avail[i];
+		avail[i].prev = &avail[i];
+		avail[i].kval = i;
+		avail[i].tag = UNUSED;
+	}	
+
+	// Updating the headers information
+	p->next = &avail[29];
+	p->prev = &avail[29];
+	p->kval = 29;
+	p->tag = FREE;
+
+	return TRUE;
 }
 
 void *buddy_malloc(size_t size)
