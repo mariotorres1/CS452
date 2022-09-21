bf92ff890f0b3fb00296b9dd1e46106942b65dd4
Mario Torres <mariotorres791@u.boisestate.edu>
2022-09-21 13:28:08 -0600

    Think I finished buddy_malloc(), needs testing still

--- p3/buddy.c
+++ p3/buddy.c
@@ -61,10 +61,95 @@ void *buddy_malloc(size_t size)
 		lgSize++;
 	}
 
-	// verify lgSize size
+	// Verify lgSize size
 	if (lgSize > 29) {
 		return NULL;
 	}
+
+	// Start looking for available block
+	for (i = lgSize; i < 30; i++) {
+		p = &avail[i];
+		while (p->next->tag != UNUSED) {
+			p = p->next;
+			if (p->tag == FREE) {
+				free = TRUE;
+				break;
+			}
+		}
+		if (free) {
+			break;
+		}
+	}
+
+	// Verify that something was found
+	if (!free) {
+		return NULL;
+	}
+
+	// Check to see if free block is at avail[lgSize]
+	if (p->kval == lgSize) {
+		struct block_header *t1, *t2;
+		p->tag = RESERVED;
+		t1 = p->next;
+		t2 = p->prev;
+		t1->prev = t2;
+		t2->next = t1;
+		retVal = (void *)((char *)p + sizeof(struct block_header));
+		return retVal;
+	}
+
+	// If not save avail level
+	j = i;
+
+	// Finding memory in block at avail level
+	r = DEFAULT_MAX_MEM_SIZE;
+	while (i < 29) {
+		r = r >> 1;
+		i++;
+	}
+
+	// Update next and prev pointers
+	while (lgSize < j) {
+		if (p->next != NULL && p->prev != NULL) {
+			p2 = p->prev;
+			p3 = p->next;
+
+			p2->next = p3;
+			p3->prev = p2;
+
+			// Since p is being used, remove
+			p->next = NULL;
+			p->prev = NULL;
+			p->tag = RESERVED;
+		}
+
+		// Adjust r and j
+		r = r >> 1;
+		j--;
+
+		// Making new header
+		new = (struct block_header *)(base + r);
+
+		// Adjusting values
+		p->kval = j;
+		new->kval = j;
+		new->tag = FREE;
+
+		// Adding to linked list
+		p2 = &avail[j];
+		p3 = p2->next;
+
+		// Adjusting pointers
+		p2->next = new;
+		new->prev = p2;
+		new->next = p3;
+		p3->prev = new;
+	}
+
+	// retVal
+	retVal = (void *)((char *)p + sizeof(struct block_header));
+	
+	return retVal;
 }
 
 void buddy_free(void *ptr) 
