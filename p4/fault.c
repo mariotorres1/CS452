0dd87a53e1c7ac4960a045653cd24925e73293d2
mariotorres1 <85824580+mariotorres1@users.noreply.github.com>
2022-10-06 20:41:21 -0600

    Added comments

--- p4/fault.c
+++ p4/fault.c
@@ -64,8 +64,11 @@ int infiniti_do_page_fault(struct infiniti_vm_area_struct *infiniti_vma, uintptr
 	pml4_table = (unsigned long)__va(cr3 & 0x000FFFFFFFFFF000);
 	pml4e = (unsigned long *)(pml4_table + (unsigned long)(((fault_addr >> 39) & 0x01ff) << 3));
 
+	// Checking the present bit of pml4e
 	if (*pml4e & 0x1) {
+		// If it's 1, do nothing
 	} else {
+		/* If not 1, allocate table and update pm14e to show that */
 		uintptr_t kernel_addr = 0;
 		kernel_addr = (uintptr_t)get_zeroed_page(GFP_KERNEL);
 		
@@ -77,27 +80,35 @@ int infiniti_do_page_fault(struct infiniti_vm_area_struct *infiniti_vma, uintptr
 		*pml4e = (unsigned long)(*pml4e | ((unsigned long)__pa(kernel_addr) & (unsigned long)0xffffffffff000));
 	}
 
+	// Updating pdp_table pdpte variables
 	pdp_table = (unsigned long)__va(*pml4e & 0x000FFFFFFFFFF000);
-    pdpte = (unsigned long *)(pdp_table + (unsigned long)(((fault_addr >> 30) & 0x01ff) << 3));
+    	pdpte = (unsigned long *)(pdp_table + (unsigned long)(((fault_addr >> 30) & 0x01ff) << 3));
 
-    if (*pdpte & 0x1) {
-    } else {
-        uintptr_t kernel_addr = 0;
-        kernel_addr = (uintptr_t)get_zeroed_page(GFP_KERNEL);
+	// Checking the present bit of pdpte
+	    if (*pdpte & 0x1) {
+		    // If it's 1, do nothing
+	    } else {
+		    /* If not 1, allocate table and update pdpte to show that */
+		uintptr_t kernel_addr = 0;
+		kernel_addr = (uintptr_t)get_zeroed_page(GFP_KERNEL);
 
-        if (!kernel_addr) {
-            return -ENOMEM;
-        }
+		if (!kernel_addr) {
+		    return -ENOMEM;
+		}
 
-        *pdpte = (unsigned long)((unsigned long)*pdpte | 0x7);
-        *pdpte = (unsigned long)(*pdpte | ((unsigned long)__pa(kernel_addr) & (unsigned long)0xffffffffff000));
-    }
+		*pdpte = (unsigned long)((unsigned long)*pdpte | 0x7);
+		*pdpte = (unsigned long)(*pdpte | ((unsigned long)__pa(kernel_addr) & (unsigned long)0xffffffffff000));
+	    }
 
+	// Updating pd_table and pde variables
     pd_table = (unsigned long)__va(*pdpte & 0x000FFFFFFFFFF000);
     pde = (unsigned long *)(pd_table + (unsigned long)(((fault_addr >> 21) & 0x01ff) << 3));
 
+	// Checking the present bit of pde
     if (*pde & 0x1) {
+	    // If it's 1, do nothing
     } else {
+	    /* If not 1, allocate table and update pde to show that */
         uintptr_t kernel_addr = 0;
         kernel_addr = (uintptr_t)get_zeroed_page(GFP_KERNEL);
 
@@ -105,29 +116,33 @@ int infiniti_do_page_fault(struct infiniti_vm_area_struct *infiniti_vma, uintptr
             return -ENOMEM;
         }
 
-        *pde = (unsigned long)((unsigned long)*pde | 0x7);
-        *pde = (unsigned long)(*pde | ((unsigned long)__pa(kernel_addr) & (unsigned long)0xffffffffff000));
-    }
+		*pde = (unsigned long)((unsigned long)*pde | 0x7);
+		*pde = (unsigned long)(*pde | ((unsigned long)__pa(kernel_addr) & (unsigned long)0xffffffffff000));
+	    }
 
-    pt_table = (unsigned long)__va(*pde & 0x000FFFFFFFFFF000);
-    pte = (unsigned long *)(pt_table + (unsigned long)(((fault_addr >> 12) & 0x01ff) << 3));
+	// Updating pt_table and pte variables
+	    pt_table = (unsigned long)__va(*pde & 0x000FFFFFFFFFF000);
+	    pte = (unsigned long *)(pt_table + (unsigned long)(((fault_addr >> 12) & 0x01ff) << 3));
 
-    if (*pte & 0x1) {
-    } else {
-        uintptr_t kernel_addr = 0;
-        kernel_addr = (uintptr_t)get_zeroed_page(GFP_KERNEL);
+	// Checking the present bit of pte
+	    if (*pte & 0x1) 
+		    // If it's 1, do nothing
+	    } else {
+		/* If not 1, allocate table and update pte to show that */
+		uintptr_t kernel_addr = 0;
+		kernel_addr = (uintptr_t)get_zeroed_page(GFP_KERNEL);
 
-        if (!kernel_addr) {
-            return -ENOMEM;
-        }
+		if (!kernel_addr) {
+		    return -ENOMEM;
+		}
 
-        *pte = (unsigned long)((unsigned long)*pte | 0x7);
-        *pte = (unsigned long)(*pte | ((unsigned long)__pa(kernel_addr) & (unsigned long)0xffffffffff000));
-    }
+		*pte = (unsigned long)((unsigned long)*pte | 0x7);
+		*pte = (unsigned long)(*pte | ((unsigned long)__pa(kernel_addr) & (unsigned long)0xffffffffff000));
+	    }
 
-	return 0;
+		return 0;
 
-}
+	}
 
 /* this function takes a user VA and free its PA as well as its kernel va. */
 void infiniti_free_pa(uintptr_t user_addr){
@@ -142,63 +157,95 @@ void infiniti_free_pa(uintptr_t user_addr){
 	pml4_table = (unsigned long)__va(cr3 & 0x000FFFFFFFFFF000);
 	pml4e = (unsigned long *)(pml4_table + (unsigned long)(((user_addr >> 39) & 0x01ff) << 3));
 
+	// Checking present bit of pml4e
 	if (*pml4e & 0x1) {
+		// If it's 1, do nothing
 	} else {
+		// Return if it's not 1
 		return;
 	}
 
+	// Updating pdp_table and pdpte variables
 	pdp_table = (unsigned long)__va(*pml4e & 0x000FFFFFFFFFF000);
 	pdpte = (unsigned long *)(pdp_table + (unsigned long)(((user_addr >> 30)&0x01ff) << 3));
 
+	// Checking present bit of pdpte
 	if (*pdpte & 0x1) {
+		// If it's 1, do nothing
 	} else {
+		// If it's not 1, return
 		return;
 	}
 
+	// Updating pd_table and pde variables
 	pd_table = (unsigned long)__va(*pdpte & 0x000FFFFFFFFFF000);
 	pde = (unsigned long *)(pd_table + (unsigned long)(((user_addr >> 21)&0x01ff) << 3));
 
+	// Checking present bit of pde
 	if (*pde & 0x1) {
+		// If it's 1, do nothing
 	} else {
+		// If it's not 1, return
 		return;
 	}
 
+	// Updating pt_table and pte variables
 	pt_table = (unsigned long)__va(*pde & 0x000FFFFFFFFFF000);
 	pte = (unsigned long *)(pt_table + (unsigned long)(((user_addr >> 12)&0x01ff) << 3));
 
+	// Checking present bit of pte
 	if (*pte & 0x1) {
+		// If it's 1, do nothing
 	} else {
+		// If it's not 1, return
 		return;
 	}
 
+	// Update kernel_addr and free it's page
 	kernel_addr = (unsigned long)__va(*pte & 0x000ffffffffff000) + (user_addr & 0xfff);
 	free_page(kernel_addr);
 
+	// Setting pte to 0
 	*pte = 0;
+	// Checking if the pt_table is free
 	if (is_entire_table_free(pt_table)) {
+		// If it is, free the page
 		free_page(pt_table);
 	} else {
+		// If it's not free, return
 		return;
 	}
 
+	// Setting pde to 0
 	*pde = 0;
+	// Checking if the pd_table is free
 	if (is_entire_table_free(pd_table)) {
+		// If it is, free the page
 		free_page(pd_table);
-	} else{
+	} else {
+		// If it's not free, return
 		return;
 	}
 
+	// Setting pdpte to 0
 	*pdpte = 0;
+	// Checking if the pdp_table is free
 	if (is_entire_table_free(pdp_table)) {
+		// If it is, free the page
 		free_page(pdp_table);
 	} else {
+		// If it's not free, return
 		return;
 	}
 
+	// Settting pml4e to 0
 	*pml4e = 0;
+	// Checking if the pml4_table is free
 	if (is_entire_table_free(pml4_table)) {
+		// If it is, free the page
 		free_page(pml4_table);
 	} else {
+		// If it's not free, return
 		return;
 	}
 }
